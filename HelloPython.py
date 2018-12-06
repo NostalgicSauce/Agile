ce467d048b7fb8d4a4c82644b66a4f8efdb55ea2
NostalgicSauce <eannalenihan1@gmail.com>
2018-12-06 17:38:16 +0000

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -1,5 +1,5 @@
 import datetime
-print("Hello Cruel world!")
+print("Hello Cruel worlds!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
